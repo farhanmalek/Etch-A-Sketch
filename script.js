7bef5b63fd40650a75c62b2fbcaa548d7aef0f63
Farhan Malek <malekf2328@gmail.com>
2023-08-30 22:30:10 +1200

    fixed reset button

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 const myContainer = document.querySelector(".container");
 
-for (let i = 0; i < 4096; i++) {
-  const div = document.createElement("div");
-  div.classList.add("square");
-  myContainer.appendChild(div);
-}
-
 const pixels = document.querySelectorAll(".square");
 const pixelArray = Array.from(pixels);
 let x = false;
@@ -15,19 +9,65 @@ let y = false;
 myContainer.addEventListener("mousedown", () => {
     x = true;
     // console.log("x true");
+    myContainer.addEventListener("mouseover", (e) => {
+        y = true;
+        // console.log("y true");
+    
+        if (x && y) {
+            e.target.style.backgroundColor = document.querySelector("input[type=color]").value;
+        }
+    })
+
+    myContainer.addEventListener("mouseup", () => {
+        x = false;
+        y = false; 
+        // console.log("x and y reset to false");
+    });
+    
 });
 
-myContainer.addEventListener("mouseover", (e) => {
-    y = true;
-    // console.log("y true");
-
-    if (x && y) {
-        e.target.style.backgroundColor = "white";
-    }
-});
-
-myContainer.addEventListener("mouseup", () => {
+myContainer.addEventListener("mouseleave", () => {
     x = false;
     y = false;
     // console.log("x and y reset to false");
 });
+
+
+// Display selected number next to slider
+
+const slider = document.querySelector(".slider");
+slider.oninput = function (e)  {
+    e.target.nextElementSibling.textContent = e.target.value;    
+}
+
+//Add functionality to grid, where we can now change the grid size based on what the slider value;
+slider.addEventListener('mouseup', () => {
+    const myValue = parseInt(document.querySelector(".slider p").textContent); // Assuming you have an element with the ID "myContainer"
+    for (let i = 0; i < Math.pow(myValue, 2); i++) {
+        const div = document.createElement("div");
+        div.classList.add("square");
+        myContainer.appendChild(div);
+    }
+
+    const pixelArray = document.querySelectorAll(".square");
+    pixelArray.forEach((pixel) => {
+        pixel.style.cssText = `width: calc(100% / ${myValue}); padding-bottom: calc(100% / ${myValue});`;
+    });
+});
+
+for (let i = 0; i < 256 ; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    myContainer.appendChild(div);
+}
+
+
+const reset = document.querySelector("#reset")
+
+reset.addEventListener('click', () => {
+    const pixels = document.querySelectorAll(".square");
+const pixelArray = Array.from(pixels);
+    pixelArray.forEach(pixel => {
+        pixel.style.cssText = "background: #333";
+    })
+})
